81c58ccb98a7f60bf9af2cea4f77e8873e30435b
mimaketi <mimaketi@gmail.com>
2024-07-09 20:44:08 -0400

    Add files via upload

---
+++ enum.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+NAME=$1
+IP=$2
+WIKIPATH=/usr/share/vimwiki/TCM-Sec
+FP=$WIKIPATH/$NAME/80.tcp.md
+if [ -z "$NAME" ]; then
+  echo "Error: NAME is empty"
+  exit 1  # Exit with an error code (optional)
+fi
+if [ -z "$IP" ]; then
+  echo "Error: IP is empty"
+  exit 1  # Exit with an error code (optional)
+fi
+if [ ! -d "$WIKIPATH/$NAME" ]; then
+  echo "PATH DOES NOT EXIST: $WIKIPATH/$NAME"
+  exit 1
+fi
+echo "## Whatweb\n" | tee -a $FP
+whatweb -a 3 http://$IP | tee -a $FP
+echo "\n\n## ffuf\n" | tee -a $FP
+ffuf -mc 200-399 -recursion -recursion-depth 2 -recursion-strategy greedy -c -w /usr/share/wordlists/dirb/big.txt -u http://$IP/FUZZ | sed '/^true$/d' | tee -a $FP
+echo "\n\n## ffuf error pages\n" | tee -a $FP
+ffuf -mc 401,403,405,500 -w /usr/share/wordlists/dirb/big.txt -u http://$IP/FUZZ | sed '/^true$/d' | tee -a $FP
+echo "\n\n## gospider\n" | tee -a $FP
+gospider -s http://$IP | tee -a $FP
+echo "\n\n## vhost search\n" | tee -a $FP
+gobuster vhost -q -u http://$IP -w /usr/share/wordlists/dirb/big.txt | tee -a $FP
+echo "Gobuster running in background..."
+echo "\n\n## nikto\n" |tee -a $FP
+nikto -host http://$IP | tee -a $FP
---
+++ nmap.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+NAME=$1
+IP=$2
+WIKIPATH=/usr/share/vimwiki/TCM-Sec
+if [ -z "$NAME" ]; then
+  echo "Error: NAME is empty"
+  exit 1  # Exit with an error code (optional)
+fi
+if [ -z "$IP" ]; then
+  echo "Error: IP is empty"
+  exit 1  # Exit with an error code (optional)
+fi
+nmap -vvvv -sT -p- $IP --min-rate=10000 --open -oN /tmp/$NAME --stats-every 5s;
+cp /usr/share/markdown.txt /tmp/$NAME.tcp;
+cat /tmp/$NAME | awk -F '[[:space:]]+|/' '/^[0-9]/ {printf "| %-5s | %-8s | %-5s | [%s](%s.tcp.md) |\n", $1, $2, $3, $4, $1}' >> /tmp/$NAME.tcp
+echo >> $WIKIPATH/$NAME/$NAME.md
+cat /tmp/$NAME.tcp >> $WIKIPATH/$NAME/$NAME.md
+
+nmap -vvvv -sU -p- $IP --max-retries=1 -T5 --min-rate=10000 --open -oN /tmp/$NAME --stats-every 5s;
+cat /tmp/$NAME | awk -F '[[:space:]]+|/' '/^[0-9]/ {printf "| %-5s | %-8s | %-5s | [%s](%s.udp.md) |\n", $1, $2, $3, $4, $1}' | grep -v 'filtered' >> /tmp/$NAME.udp
+cat /tmp/$NAME.udp >> $WIKIPATH/$NAME/$NAME.md
+echo "" >> $WIKIPATH/$NAME/$NAME.md
